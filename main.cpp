ecdf50b0a9870805029f204b426c41005daab0ba
Set <s--e--t@yandex.ru>
2024-07-18 16:46:54 +0500

    - исправлен вывод процессов, запущенных от админа

--- main.cpp
+++ main.cpp
@@ -17,9 +17,7 @@ BOOL IsTaskbarWindow(HWND hwnd, HWND desktop) {
     HWND parent = GetParent(hwnd);
     int level;
 
-    //
     // determine windows' level. 0=desktop, 1=toplevel, 2+ = child
-    //
     if (hwnd == desktop) level = 0;
     else if ((parent == owner) || (parent == desktop)) level = 1;
     else {
@@ -60,22 +58,6 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
 }
 
 int main() {
-//    std::ofstream logfile("log.txt"); // создаем объект для записи в файл log.txt
-//    std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
-//
-//    std::string log = "0";
-//    if (argc >= 2)
-//        log = argv[2];
-//    std::cout << "Log option = " << log << std::endl;
-//
-//    if (log == "1") {
-//        if (logfile.is_open())  // проверяем, успешно ли открыт файл
-//            // Сохраняем стандартный вывод в переменную
-//            std::cout.rdbuf(logfile.rdbuf()); // перенаправляем стандартный вывод в файл
-//        else {
-//            std::cerr << "Unable to open file log.txt" << std::endl;
-//        }
-//    }
     int explorerCount = 0;
     bool dontPrint = FALSE;
     HWND desktop = GetDesktopWindow();
@@ -129,47 +111,5 @@ int main() {
             CloseHandle(hProcess);
         }
     }
-
-//    HWND hwnd = FindWindow(NULL, NULL);
-//    while (hwnd) {
-//        DWORD pid;
-//        GetWindowThreadProcessId(hwnd, &pid);
-//        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pid);
-//
-//        TCHAR szExe[MAX_PATH + 1];
-//        DWORD lpdwSize = MAX_PATH;
-//
-//        [[maybe_unused]] BOOL bFile = QueryFullProcessImageName(hProcess, 0, szExe, &lpdwSize);
-//
-//        if (endsWith(szExe, "explorer.exe"))
-//            explorerCount++;
-//
-//        dontPrint = beginsWith(szExe, "C:\\Windows\\");
-//
-//        if (IsTaskbarWindow(hwnd, desktop) && !dontPrint || explorerCount == 1) {
-//            if (explorerCount == 1)
-//                explorerCount++;
-//
-//            CFileVersionInfo data;
-//            data.Create(szExe);
-//
-//            std::cout <<
-//                      hwnd << " ## " <<
-//                      szExe << " ## " <<
-//                      data.GetFileVersion().c_str() << " ## " <<
-//                      data.GetProductVersion().c_str() << " ## " <<
-//                      data.GetFileDescription().c_str() <<
-//                      std::endl;
-//
-//            CloseHandle(hProcess);
-//        }
-//        hwnd = FindWindowEx(NULL, hwnd, NULL, NULL);
-//    }
-
-//    if (log == "1") {
-//        std::cout.rdbuf(oldCoutStreamBuf);
-//        logfile.close();
-//    }
-
     return 0;
 }
